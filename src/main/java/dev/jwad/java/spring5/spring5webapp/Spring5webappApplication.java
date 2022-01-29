d83ba9fea8075b1770a8d9e0f7299fabb0369d29
jawaddle <hjawaddle12@gmail.com>
2022-01-29 11:38:15 -0700

    Initial commit with Spring Initializr dependencies

---
+++ src/main/java/dev/jwad/java/spring5/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,13 @@
+package dev.jwad.java.spring5.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jwad/java/spring5/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jwad.java.spring5.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
