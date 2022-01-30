707747ad28ed6308a142f2a58b72ed7bf34ec17e
jawaddle <hjawaddle12@gmail.com>
2022-01-30 10:11:35 -0700

    Added Thymeleaf templates

---
+++ src/main/java/dev/jwad/java/spring5/spring5webapp/controllers/BookController.java
@@ -0,0 +1,23 @@
+package dev.jwad.java.spring5.spring5webapp.controllers;
+
+import dev.jwad.java.spring5.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books",bookRepository.findAll());
+        return "books/list";
+    }
+}
